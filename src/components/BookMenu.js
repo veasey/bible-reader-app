ee03a067dbdac4219d74f47168e2439097c2cb88
Clint Veasey <clintvee@gmail.com>
2025-01-21 21:36:08 +0000

    basic styling and toggle logic added to book list for book menu

--- src/components/BookMenu.js
+++ src/components/BookMenu.js
@@ -1,33 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './BookMenu.css';
 import { books } from '../constants/books.js';
 
 const BookMenu = ({ message }) => {
 
-  const oldTestamentBooks = books.slice(0,40);
-  const newTestamentBooks = books.slice(40);
+    const [isOpen, setIsOpen] = useState(false);
 
-  return (
-    <div class='book-menu-container'>
-      <h1>Menu</h1>
-      <div>
-        <h2>Old Testament</h2>
-        <ul>
-          {oldTestamentBooks.map((item, index) => (
-            <li key={`cat1-${index}`}>{item.name}</li>
-          ))}
-        </ul>
-      </div>
-      <div>
-        <h2>New Testament</h2>
-        <ul>
-          {newTestamentBooks.map((item, index) => (
-            <li key={`cat2-${index}`}>{item.name}</li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    const oldTestamentBooks = books.slice(0,40);
+    const newTestamentBooks = books.slice(40);
+
+    return (
+        <div className='book-menu-container'>
+
+        {/* Burger Icon */}
+        <button className="burger-icon" onClick={toggleMenu}>
+            {isOpen ? "✕" : "☰"}
+        </button>
+
+        {isOpen && (
+            <div>
+                <h1>Menu</h1>
+                <div className="book-menu-lists">
+                    <div>
+                        <h2>Old Testament</h2>
+                        <ul>
+                            {oldTestamentBooks.map((item, index) => (
+                                <li key={`cat1-${index}`}>{item.name}</li>
+                            ))}
+                        </ul>
+                    </div>
+                    <div>
+                        <h2>New Testament</h2>
+                        <ul>
+                            {newTestamentBooks.map((item, index) => (
+                                <li key={`cat2-${index}`}>{item.name}</li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        )}
+        </div>
+    );
 };
 
 export default BookMenu;
