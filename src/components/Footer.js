d463fa3d8b0cbc1033a8d719f242bb3a831ddbb7
Clint Veasey <clintvee@gmail.com>
2025-01-29 10:10:56 +0000

    add a footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React  from 'react';
+
+const Footer = () => {
+    
+    return (
+        <div>
+            <p>This app is completely free to use and distributed under an open license. You may share, copy, and use the content for personal and non-commercial purposes. No registration, ads, or in-app purchases—just God’s Word, freely available.</p>
+            <p>"Freely you have received; freely give." – Matthew 10:8</p>
+        </div>
+    );
+}
+
+export default Footer;
