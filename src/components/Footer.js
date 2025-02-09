5229d442d3a653d375bafa4bfaa632c3d2585e8f
Clint Veasey <clintvee@gmail.com>
2025-02-09 13:42:16 +0000

    tweak footer text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,9 @@ const Footer = () => {
     
     return (
         <div className="footer">
-            <p>This app is completely free to use and distributed under an open license. You may share, copy, and use the content for personal and non-commercial purposes. No registration, ads, or in-app purchases—just God’s Word, freely available.</p>
+            <p>This app is completely free to use and distributed under an open license. You may 
+            share, copy, and use the content for personal and non-commercial purposes. No 
+            registration, ads, or in-app purchases.</p>
             <p><span className="quote">"Freely you have received; freely give."</span> – Matthew 10:8</p>
         </div>
     );
